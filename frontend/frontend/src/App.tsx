596e4b1009738a63a3bd77fe0ef0a18f99d6e863
David Alexandru <fanefaraon696@gmail.com>
2022-11-18 16:17:08 +0200

    ADDED: installed react-routed-dom package

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "../src/pages/LoginPage/LoginPage";
 
 function App() {
   const [myState, setMyState] = useState<string>("0");
@@ -22,30 +25,41 @@ function App() {
   }, [counter]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button
-          ref={buttonRef}
-          onClick={() => {
-            setCounter(counter + 1);
-          }}
-        >
-          0
-        </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route
+        path="/home"
+        element={
+          <>
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                  Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <Button>click</Button>
+                <button
+                  ref={buttonRef}
+                  onClick={() => {
+                    setCounter(counter + 1);
+                  }}
+                >
+                  0
+                </button>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </a>
+              </header>
+            </div>
+          </>
+        }
+      ></Route>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
