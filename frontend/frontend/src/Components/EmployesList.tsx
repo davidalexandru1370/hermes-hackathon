3297059b8bf91a07e357418be2d2645cf5daf33d
Tiron Raul-Bogdan <115061485+PotatoOverlord22@users.noreply.github.com>
2022-11-19 22:57:57 +0200

    Colored Rows (#16)
    * stash
    
    * ADDED: colored rows

--- frontend/frontend/src/Components/EmployesList.tsx
+++ frontend/frontend/src/Components/EmployesList.tsx
@@ -8,28 +8,47 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { IEmployee } from "../Model/IEmployee";
+// const d = new Date(2018, 11, 24, 10, 33, 30, 0);
 
 const EmployesList = () => {
-  const rows: IEmployee[] = [
+
+    const rows: IEmployee[] = [
     { id: "1", name: "david" },
     { id: "1", name: "david" },
     {
       id: "1",
       name: "david",
-      documents: [{ id: "1", title: "analize medicale", date: "24/07/2022" }],
+      documents: [{ id: "1", title: "analize medicale", date : new Date(2018,2,3) }],
     },
     {
       id: "1",
       name: "david",
-      documents: [{ id: "1", title: "analize medicale", date: "25/06/2022" }],
+      documents: [{ id: "1", title: "analize medicale", date: new Date(2022,1,3) }],
+    },
+    {
+      id: "1",
+      name: "david",
+      documents: [{ id: "1", title: "analize medicale", date: new Date(2022,7,23) }],
     },
   ];
+
+  function colorRow(documentDate: Date) {
+    const currentDate = new Date();
+    const dateDifference = new Date(currentDate.getTime() - documentDate.getTime());
+    if (dateDifference.getUTCFullYear() - 1970 >= 1)
+      return "#b30000"
+    else if (dateDifference.getUTCMonth() > 6)
+      return "#e67300"
+    else
+      return "#90EE90"
+
+  }
 
   return (
     <TableContainer component={Paper} sx={{ maxWidth: '100%' }}>
       <Table sx={{ minWidth: 650, maxWidth: '100%' }} aria-label="simple table">
         <TableHead>
-          <TableRow>
+          <TableRow >
             <TableCell></TableCell>
             <TableCell align="right">Name</TableCell>
             <TableCell align="right">Document name</TableCell>
@@ -44,6 +63,7 @@ const EmployesList = () => {
                 <TableRow
                   key={row.name}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  style={{backgroundColor: `${colorRow(row.documents[0].date)}`}}
                 >
                   <TableCell component="th" scope="row">
                     <RemoveRedEyeIcon
@@ -58,7 +78,7 @@ const EmployesList = () => {
                   </TableCell>
                   <TableCell align="right">{row.name}</TableCell>
                   <TableCell align="right">{row.documents[0].title}</TableCell>
-                  <TableCell align="right">{row.documents[0].date}</TableCell>
+                  <TableCell align="right">{row.documents[0].date.toLocaleDateString('en-UK')}</TableCell>
                   <TableCell align="right">
                     <DoNotDisturbOnIcon
                       sx={{
@@ -69,6 +89,7 @@ const EmployesList = () => {
                       }}
                     />
                   </TableCell>
+
                 </TableRow>
               )
           )}
