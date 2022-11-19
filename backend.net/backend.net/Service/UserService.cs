0ae1e2c5ad2cb04a71b1ba5ed9536c2e791bb010
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-11-19 15:08:05 +0200

    Pd.be (#7)
    * test
    
    * ADDED: backend .net
    
    * ADDED user model and dbcontext
    
    * added database migration and created user table
    
    * added login methods

---
+++ backend.net/backend.net/Service/UserService.cs
@@ -0,0 +1,35 @@
+﻿using backend.net.Model;
+using backend.net.Model.DTOs;
+using backend.net.Repository;
+using BCrypt;
+namespace backend.net.Service
+{
+    public class UserService : IUserService
+    {
+        private IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> Login(UserDto user)
+        {
+            var _user = _userRepository.GetByEmail(user.Email);
+
+            if (BCrypt.Net.BCrypt.Verify(user.Password,_user!.Password))
+            {
+                throw new Exception("Email does not exists!");
+            }
+
+            return _user;
+        }
+
+        public async Task Register(UserDto user)
+        {
+            user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            await _userRepository.Add(user);
+        }
+
+    }
+}
