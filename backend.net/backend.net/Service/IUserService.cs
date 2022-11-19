3c636609e521d4d75ac39f1677a8f98e43d29aaf
davidalexandru1370 <68737671+davidalexandru1370@users.noreply.github.com>
2022-11-19 15:08:05 +0200

    Pd.be (#7)
    * test
    
    * ADDED: backend .net
    
    * ADDED user model and dbcontext
    
    * added database migration and created user table
    
    * added login methods

---
+++ backend.net/backend.net/Service/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using backend.net.Model;
+using backend.net.Model.DTOs;
+
+namespace backend.net.Service
+{
+    public interface IUserService
+    {
+        public Task<User> Login(UserDto user);
+
+        public Task Register(UserDto user);
+
+    }
+}
